312390e37394518185790f738d01740677647ee6
skelleher2 <52292132+skelleher2@users.noreply.github.com>
2020-07-30 19:31:58 -0500

    Update Signin.js

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('https://git.heroku.com/facial-recognition-machine.git/signin', {
+    fetch('https://facial-recognition-machine.herokuapp.com/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
