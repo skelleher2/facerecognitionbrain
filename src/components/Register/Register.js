2ac624a29961a992aa7bcbf5b4cb8a0f76e70117
skelleher2 <52292132+skelleher2@users.noreply.github.com>
2020-07-30 19:31:37 -0500

    Update Register.js

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('https://git.heroku.com/facial-recognition-machine.git/register', {
+    fetch('https://facial-recognition-machine.herokuapp.com/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
