1cdde54186f31bc118a98f098e2b98d3b3ea5a77
skelleher2 <seankelleher2@gmail.com>
2020-08-04 19:30:27 -0500

    App error adding curly brackets

--- src/App.js
+++ src/App.js
@@ -105,7 +105,7 @@ class App extends Component {
 
   onRouteChange = (route) => {
     if (route === 'signout') {
-      this.setState(isSignedIn: false)
+      this.setState({isSignedIn: false})
     } else if (route === 'home') {
       this.setState({isSignedIn: true})
     }
